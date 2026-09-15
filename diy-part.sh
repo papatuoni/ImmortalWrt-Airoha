8bbc1b56e90d36fd4d5c5573b8e149793bbcb148
papatuoni <105186707+papatuoni@users.noreply.github.com>
2026-09-16 02:13:55 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # turboacc
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
